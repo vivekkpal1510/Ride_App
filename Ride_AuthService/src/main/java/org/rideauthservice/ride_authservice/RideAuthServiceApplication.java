487be58204370dceff53b78bd06a4f5cf4f77b09
vivekkpal1510 <vkpal1510@gmail.com>
2026-01-06 15:34:35 +0530

    Setup

---
+++ Ride_AuthService/src/main/java/org/rideauthservice/ride_authservice/RideAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package org.rideauthservice.ride_authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RideAuthServiceApplication.class, args);
+    }
+
+}
