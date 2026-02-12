37fa4d6e4a6e4acc340e043fa87cf91546beb4a0
vivekkpal1510 <vkpal1510@gmail.com>
2026-02-12 22:36:04 +0530

    Adding kafka for booking service

--- BookingService/src/main/java/org/example/bookingservice/controllers/BookingController.java
+++ BookingService/src/main/java/org/example/bookingservice/controllers/BookingController.java
@@ -26,7 +26,7 @@ public class BookingController {
         return new ResponseEntity<>(bookingService.createBooking(createBookingDto), HttpStatus.CREATED);
     }
 
-    @PatchMapping("/{bookingId}")
+    @PostMapping("/{bookingId}")
     public ResponseEntity<UpdateBookingResponseDto> updateBooking(@RequestBody UpdateBookingRequestDto requestDto, @PathVariable Long bookingId) {
         System.out.println(bookingId +" " + requestDto +" "+ "hrll");
         return new ResponseEntity<>(bookingService.updateBooking(requestDto, bookingId), HttpStatus.OK);
